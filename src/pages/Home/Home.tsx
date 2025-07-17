a8eaf3e9b19744729f275819c6563fbe336158db
tcunha2004 <tcunha2004@gmail.com>
2025-07-17 11:36:35 -0300

    #D054: criando uma lista de ciclos e criando o ciclo ativo naquele momento

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,21 +9,45 @@ import {
   TaskInput,
 } from "./styles";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 function Home() {
   const { register, watch, handleSubmit, reset } = useForm<FormData>(); // useForm: controlador do formulario
 
   const taskWatcher = watch("task"); // useForm esta observando os inputs registrados
-  const minutesWatcher = watch("minutesAmount");
+  const minutesWatcher = watch("minutesAmount"); // useForm esta observando os inputs registrados
+  // ---------------------------------------------------------------------------------------------------------------
+  interface Cycle {
+    id: string;
+    task: string;
+    minutesAmount: number;
+  }
 
+  const [cycles, setCycles] = useState<Cycle[]>([]); // o useState armazenará uma lista de ciclos
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null); // id do ciclo ativo no momento (começa com null)
+  // ---------------------------------------------------------------------------------------------------------------
   interface FormData {
     task: string;
     minutesAmount: number;
   }
 
   function handleFormSubmit(data: FormData) {
+    // crio um novo ciclo (seguindo a interface de como deve ser um ciclo)
+    const newCycle: Cycle = {
+      id: String(new Date().getTime()),
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    };
+
+    setCycles((prevState) => [...prevState, newCycle]); // concateno o que tinha antes com o que vou add
+    setActiveCycleId(newCycle.id);
+
     reset(); // reseta o formulário quando der o submit
   }
+
+  const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId);
+
+  console.log(activeCycle);
 
   return (
     <HomeContainer>
